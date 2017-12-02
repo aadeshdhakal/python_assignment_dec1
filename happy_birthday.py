f9443865b97e92528d860f496bbad7157e33804d
aadeshdhakal <aadeshdhakal@gmail.com>
2017-12-02 19:00:56 +0545

    birthday song.

---
+++ happy_birthday.py
@@ -0,0 +1,6 @@
+def birthday(f_name):
+    print("{0} {1} {2} {3}\n{0} {1} {2} {3}\n{0} {1} {4} {5}\n{0} {1} {2} {3}!!".format("happy", "birthday", "to", "you", "dear", f_name))
+
+
+name = input("Enter your friend's name: ")
+birthday(name)
